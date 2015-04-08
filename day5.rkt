fd6a9a56b963116eaf80ea8d991b9318895ad188
Dave Musicant (Carleton VM) <dmusican@carleton.edu>
2015-04-08 13:43:33 -0500

    day 5

--- day5.rkt
+++ day5.rkt
@@ -8,3 +8,15 @@
 (foldl cons '() '(1 2 3))
 (foldr cons '() '(1 2 3))
 (foldl + 5 '(1 2 3))
+
+(define my-map
+  (lambda (f lst)
+    (if (null? lst) '()
+        (cons (f (car lst)) (my-map f (cdr lst))))))
+
+(define my-foldl
+  (lambda (f init lst)
+    (if (null? lst) init
+        (my-foldl f (f (car lst) init) (cdr lst)))))
+
+(my-map addOne '(1 8 3 5))
